7d637079a8daae53e5c1f8df7793798cd7b3a87c
ariegafni <ariegafni18@gmail.com>
2025-08-27 22:46:09 +0300

    WIP: save changes before switching branch

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,28 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3002;
+const port = parseInt(process.env.PORT || "3002", 10);
 
-app.use(cors({
-  origin: ["http://localhost:3002", "http://127.0.0.1:3002"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost",
+      "http://localhost:3002",
+      "http://127.0.0.1:3002",
+      "http://10.0.2.2:3005",
+      "capacitor://localhost"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use("/api", router);
 
 connectDB();
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${port}`);
 });
